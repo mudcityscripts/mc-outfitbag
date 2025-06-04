7610e38e789bc375d2be75e70c373141d55fcc52
mudcityscripts <classicdrood@gmail.com>
2025-06-04 07:11:49 -0500

    Add files via upload

---
+++ mc-outfitbag/config.lua
@@ -0,0 +1,6 @@
+Config = {}
+
+Config.TargetLabel = 'Open Outfit Bag'
+Config.TargetIcon = 'fas fa-suitcase'
+Config.AnimDict = 'amb@world_human_bum_digging@male@base'
+Config.AnimName = 'base'
---
+++ mc-outfitbag/fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'MudCityScripts'
+description 'Outfit Bag for QBCore with qb-target and illenium-appearance'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+shared_scripts {
+    'config.lua'
+}
+
+dependencies {
+    'qb-core',
+    'qb-target',
+    'ox_inventory',
+    'illenium-appearance'
+}
